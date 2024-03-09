8251940e51037325bf1f7a4e799ff871d0f64dee
Budova Diana <dianachampions011@gmail.com>
2024-03-09 21:24:57 +0200

    first commit.

---
+++ js/footer/clickCallMe.js
@@ -0,0 +1,28 @@
+var intervalId;
+
+function updateTimer() {
+    currentSeconds--;
+    var secondsText = document.getElementById("seconds");
+    if(currentSeconds<=0){
+        clearInterval(intervalId);
+    }
+    secondsText.textContent=currentSeconds.toString().padStart(2,"0");
+}
+
+function startTimer() {
+    intervalId = setInterval(updateTimer, 1000);
+}
+
+function resetTimer() {
+    clearInterval(intervalId);
+    currentSeconds=maxSeconds;
+    document.getElementById("seconds").textContent = maxSeconds.toString().padStart(2,"0");
+}
+
+const maxSeconds = 29;
+var currentSeconds=maxSeconds;
+
+document.getElementById("resetBtn").addEventListener("click", function () {
+    resetTimer();
+    startTimer();
+});
