3cd0e1c43e89a7590eb0b0d446cc346df451b3f2
Platon Holovetskyi <platongolov2@email.com>
2024-03-09 17:05:27 +0200

    new-version

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let intervalId;
+
+const updateTimer = () => {
+    currentSeconds--;
+    let secondsText = document.getElementById("seconds");
+    if (currentSeconds <= 0) {
+        clearInterval(intervalId);
+        //resetTimer();
+    }
+    secondsText.textContent = currentSeconds.toString().padStart(2, "0");
+}
+
+const startTimer = () => {
+    intervalId = setInterval(updateTimer, 1000);
+}
+
+const resetTimer = () => {
+    clearInterval(intervalId);
+    currentSeconds = maxSeconds;
+    document.getElementById("seconds").textContent = maxSeconds.toString().padStart(2, "0");
+}
+
+const maxSeconds = 29;
+let currentSeconds = maxSeconds;
+
+document.getElementById("resetBtn").addEventListener("click", function () {
+    resetTimer();
+    startTimer();
+});
