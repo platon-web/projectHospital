933535d9aa28d14dd1a44e76a05d7b28eeec732f
Platon Holovetskyi <platongolov2@email.com>
2024-03-13 16:11:10 +0200

    -

--- js/footer/mediaWidth.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("resize", phone = () => {
-    if (window.innerWidth < 962) {
-        this.document.getElementById("phone").placeholder = "+38 (___) ___-__-__*";
-    } else {
-        this.document.getElementById("phone").placeholder = "Ваш номер телефона";
-    }
-});
